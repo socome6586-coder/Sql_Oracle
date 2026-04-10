c6029b4ee1ea92334ce67ea2fc70bfe4373e25db
socome6586-coder/Seok-dev <socome6586@gmail.com>
2026-04-10 17:02:50 +0900

    2026-04-10-2 날짜함수, 문자함수

--- hr20260410.sql
+++ hr20260410.sql
@@ -262,11 +262,230 @@ VIEW : 뷰 -- SQL 문을 저장해 놓고 TABLE 처럼 호출해서 사용하는
 
 2) 일반적인 VIEW -> 영구저장 된 객체
 
-    VIEW 생성 - 영구 보관
+    VIEW 생성 - 영구 보관 -- OR REPLACE : ORACLE에서만 사용 가능
+    CREATE OR REPLACE VIEW "HR"."VIEW_EMP" ("사번","이름","이메일","전화")
+    AS
+        SELECT EMPLOYEE_ID                     사번,
+               FIRST_NAME || ' ' || LAST_NAME  이름,
+               EMAIL      || '@GREEN.COM'      이메일,
+               PHONE_NUMBER                    전화
+        FROM   EMPLOYEES
+        ORDER BY 이름;
     
+SELECT * 
+FROM   VIEW_EMP
+WHERE  이름 LIKE '%King%';
+
+----------------------------------------------------------------------
+2) WITH  -- 가상의 TABLE 생성
+    WITH A ("사번","이름","이메일","전화")
+    AS (
+        SELECT EMPLOYEE_ID                     사번,
+               FIRST_NAME || ' ' || LAST_NAME  이름,
+               EMAIL      || '@GREEN.COM'      이메일,
+               PHONE_NUMBER                    전화
+        FROM   EMPLOYEES
+        ORDER BY 이름
+    )
+    SELECT * FROM A;
+
+----------------------------------------------------------------------
+SELF JOIN 
+-- 직원번호, 직속상사번호
+SELECT EMPLOYEE_ID   직원번호,
+       MANAGER_ID    직속상사번호
+FROM   EMPLOYEES;
+
+-- 직원이름, 직속상사이름
+-- 상사정보 : E1, 부하정보 : E2 테이블 복사
+SELECT e2.FIRST_NAME || ' ' || e2.LAST_NAME 직원이름, 
+       e1.FIRST_NAME || ' ' || e1.LAST_NAME 직속상사이름
+FROM   EMPLOYEES e1, EMPLOYEES e2 
+WHERE  e1.EMPLOYEE_ID = e2.MANAGER_ID
+ORDER BY e1.EMPLOYEE_ID ASC; -- 사장이 출력되지 않는다
+
+SELECT e2.FIRST_NAME || ' ' || e2.LAST_NAME 직원이름, 
+       e1.FIRST_NAME || ' ' || e1.LAST_NAME 직속상사이름
+FROM   EMPLOYEES e1 RIGHT JOIN EMPLOYEES e2  ON  e1.EMPLOYEE_ID = e2.MANAGER_ID
+ORDER BY e1.EMPLOYEE_ID ASC; -- 모든 직원 정보 : KING, 보검, 리나
+
+----------------------------------------------------------------------
+-- 계층형 쿼리, CASCADING
+계층형 쿼리 : HIRERACHY
+
+LEVEL : 예약어, 계층형 쿼리의 레벨을 구하는 예약어
+직원번호, 직원명, 레벨, 부서명
+
+SELECT e.EMPLOYEE_ID                                                   직원번호, 
+       LPAD(' ', 3 * (LEVEL-1)) || e.FIRST_NAME || ' ' || e.LAST_NAME  직원명,
+       LEVEL, 
+       d.DEPARTMENT_NAME                   부서명
+FROM   EMPLOYEES e JOIN DEPARTMENTS d  
+ON     e.DEPARTMENT_ID = d.DEPARTMENT_ID
+START WITH e.MANAGER_ID IS NULL
+CONNECT BY PRIOR e.EMPLOYEE_ID = e.MANAGER_ID;
+
+----------------------------------------------------------------------
+EQUI JOIN, 등가조인 : 조인 조건이 = 인 것들
+NON-EQUI JOIN, 비등가 조인 : 조인 조건이 = 이 아닌 것
+
+직원등급
+월급            등급
+20000    초과     S
+15001 ~ 20000     A
+10001 ~ 15000     B
+ 5001 ~ 10000     C
+ 3001 ~  5000     D
+    0 ~  3000     E
+    
+직원번호 직원명 월급 등급
+SELECT EMPLOYEE_ID                            직원번호,
+       FIRST_NAME || ' ' || LAST_NAME         직원명,
+       SALARY                                 월급,
+       CASE
+         WHEN SALARY > 20000                 THEN 'S'
+         WHEN SALARY BETWEEN 15001 AND 20000 THEN 'A'
+         WHEN SALARY BETWEEN 10001 AND 15000 THEN 'B'
+         WHEN SALARY BETWEEN  5001 AND 10000 THEN 'C'
+         WHEN SALARY BETWEEN  3001 AND  5000 THEN 'D'
+         WHEN SALARY BETWEEN     0 AND  3000 THEN 'E'
+         ELSE                                     '등급 없음'
+       END                                    등급
+FROM   EMPLOYEES;
+
+-- 등급 테이블 생성
+DROP TABLE SALGRADE;
+
+CREATE TABLE SALGRADE
+(
+    GRADE VARCHAR2(1) PRIMARY KEY,
+    LOSAL NUMBER(11),
+    HISAL NUMBER(11)
+);
+
+INSERT INTO SALGRADE VALUES ( 'S', 20001, 99999999999 );
+INSERT INTO SALGRADE VALUES ( 'A', 15001, 20000 );
+INSERT INTO SALGRADE VALUES ( 'B', 10001, 15000 );
+INSERT INTO SALGRADE VALUES ( 'C',  5001, 10000 );
+INSERT INTO SALGRADE VALUES ( 'D',  3001,  5000 );
+INSERT INTO SALGRADE VALUES ( 'E',     0,  3000 );
+COMMIT;
 
 
+직원번호 직원명 월급 등급
+SELECT e.EMPLOYEE_ID                    직원번호,
+       e.FIRST_NAME || ' ' || LAST_NAME 직원명,
+       e.SALARY                         월급,
+       NVL(sg.GRADE, '등급없음')        등급
+FROM   EMPLOYEES e LEFT JOIN SALGRADE sg
+ON     e.SALARY BETWEEN sg.LOSAL AND sg.HISAL
+ORDER BY e.EMPLOYEE_ID ASC;
+
+----------------------------------------------------------------------
+-- 분석함수와 WINDOW 함수
+1. ROW_NUMBER() : 줄 번호 출력
+2. RANK()       : 석차 (1,2,3,4,5,6,7...)
+3. DENSE_RANK() : 석차 (1,2,2,3,4,5,5,6...)
+4. NTILE()      : 그룹으로 분류
+5. LISTAGG()    : 
+
+DESC NULLS LAST   -- null이 가장 마지막에 출력
+DESC NULLS FIRST  -- null이 가장 위에 출력
+
+1. ROW_NUMBER() : 페이징 기법
+전체자료
+SELECT   EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY
+FROM     EMPLOYEES
+ORDER BY SALARY DESC NULLS LAST;
 
 
+자료를 10개만 출력 - 페이징 기술
+1) OLD 문법 : ROWNUM -- 의사(psuedo)칼럼
+SELECT    ROWNUM, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY
+FROM      EMPLOYEES
+-- WHERE    ROWNUM BETWEEN 1 AND 10;
+ORDER BY  SALARY DESC NULLS LAST;
+
+SELECT ROWNUM, EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY
+FROM (
+        SELECT    EMPLOYEE_ID, FIRST_NAME, LAST_NAME, SALARY
+        FROM      EMPLOYEES
+        ORDER BY  SALARY DESC NULLS LAST
+)T;
 
 
+2) ANSI 문법 : ROW_NUMBER() -- 11G
+SELECT * FROM
+(
+        SELECT    ROW_NUMBER() OVER(ORDER BY SALARY DESC NULLS LAST) RN,
+                  EMPLOYEE_ID, 
+                  FIRST_NAME, 
+                  LAST_NAME, 
+                  SALARY
+        FROM      EMPLOYEES
+) T
+WHERE T.RN BETWEEN 11 AND 20;
+
+
+3) ORLCLE 12C 부터는 OFFSET
+SELECT *
+FROM     EMPLOYEES
+ORDER BY SALARY DESC NULLS LAST
+OFFSET   11 ROWS FETCH NEXT 10 ROWS ONLY;
+ -- 11 부터 10개 : ROW_NUMBER 보다 속도가 빠르다
+ 
+ -----------------------------------------------------------------
+2. RANK()       : 석차 (1,2,3,4,5,6,7...)
+
+월급순으로 석차를 출력
+사번, 이름, 월급, 석차
+SELECT EMPLOYEE_ID                                         사번,
+       FIRST_NAME || ' ' || LAST_NAME                      이름,
+       SALARY                                              월급,
+       DENSE_RANK() OVER (ORDER BY SALARY DESC NULLS LAST) 석차
+FROM   EMPLOYEES;
+
+
+월급순으로 석차를 출력(1~10등까지)
+SELECT * FROM
+(
+        SELECT EMPLOYEE_ID                                         사번,
+               FIRST_NAME || ' ' || LAST_NAME                      이름,
+               SALARY                                              월급,
+               RANK() OVER (ORDER BY SALARY DESC NULLS LAST) 석차
+        FROM   EMPLOYEES
+) T
+WHERE  T.석차 BETWEEN 1 AND 10;
+
+
+3. DENSE_RANK() : 석차 (1,2,2,3,4,5,5,6...)
+ 
+SELECT * FROM
+(
+        SELECT EMPLOYEE_ID                                         사번,
+               FIRST_NAME || ' ' || LAST_NAME                      이름,
+               SALARY                                              월급,
+               DENSE_RANK() OVER (ORDER BY SALARY DESC NULLS LAST) 석차
+        FROM   EMPLOYEES
+) T
+WHERE  T.석차 BETWEEN 1 AND 10;
+ 
+------------------------------------------------------------------
+LISTAGG() : 여러줄을 한 줄 짜리 문자열로 변경
+
+SELECT DEPARTMENT_ID          FROM   EMPLOYEES;
+
+SELECT DISTINCT DEPARTMENT_ID FROM   EMPLOYEES;
+
+SELECT LISTAGG(DISTINCT DEPARTMENT_ID) FROM   EMPLOYEES; 
+
+
+SELECT LISTAGG(DISTINCT DEPARTMENT_ID, ',')
+FROM   EMPLOYEES;
+ 
+SELECT LISTAGG(DISTINCT DEPARTMENT_ID, ',')
+WITHIN GROUP(ORDER BY DEPARTMENT_ID DESC)    -- WITHIN : 정렬방식
+FROM   EMPLOYEES;
+ 
+ 
+ 
